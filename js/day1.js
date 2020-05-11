a69031a2c809dec07e44674f5ad812a0ad8e24cf
saurabhkkk <mishrasaurabh01114@gmail.com>
2020-05-11 20:20:57 +0530

    DAY 1 OF ASSIGNMENT

---
+++ js/day1.js
@@ -0,0 +1,16 @@
+function greatest(a,b,c)
+{
+    if(a>b&&a>c)
+    {
+        console.log("FirstNUmber is greatest")
+    }
+    else if(b>a&&b>c)
+    {
+        console.log("second number is greatest");
+    }
+    else if(c>a&&c>b)
+    {
+        console.log("third number is greatest");
+    }
+}
+greatest(19,12,56);
